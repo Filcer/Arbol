ee2dd6ca6fe15931e4a5bec11bb30e4a10d1eb30
Filcer <gabfiltzer@gmail.com>
2016-10-08 01:20:36 -0500

    Update Arbol.h

--- include/Arbol.h
+++ include/Arbol.h
@@ -1,3 +1,11 @@
+/* Nombres: Gabriel Filtzer Gorbach
+	    Daniel Ponce de León Súchil
+   
+   Descripción: En esta clase se encuentran los métodos necesarios para buscar un elemento en el arbol, y para agregar y 
+   eliminar un nodo deseado. Además también está definida la búsqueda recursiva (BusqRec), la cuál simplifica el proceso 
+   de busqueda de un dato. 
+	    */
+
 #include <stack>
 #include "NodoA.h"
 
